c60d5c3fb8c7c294b3d3011daa321f3f07e01da6
HarryLuke1415 <harryluke1415@gmail.com>
2020-10-27 19:25:04 +1100

    First push

---
+++ src/main/kotlin/com/library/libraryBookInterface/LibraryBookApplication.kt
@@ -0,0 +1,11 @@
+package com.library.libraryBookInterface
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibraryBookInterfaceApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibraryBookInterfaceApplication>(*args)
+}
---
+++ src/test/kotlin/com/library/libraryBookInterface/LibraryBookInterfaceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.library.libraryBookInterface
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LibraryBookInterfaceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
